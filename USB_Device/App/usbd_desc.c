25556e2bf0b6e35db72bed1dfdd9b02c9a9cb0bc
Giulio Moro <giuliomoro@yahoo.it>
2023-06-27 12:44:27 +0200

    Updated USB device descriptors

--- USB_Device/App/usbd_desc.c
+++ USB_Device/App/usbd_desc.c
@@ -63,13 +63,14 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     0xbe1a
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID     22336
-#define USBD_PRODUCT_STRING     "TrillRack-rev1"
-#define USBD_CONFIGURATION_STRING     "CDC Config"
-#define USBD_INTERFACE_STRING     "CDC Interface"
+#define USBD_MANUFACTURER_STRING     "AIL"
+#define USBD_PID     61155
+extern char stringId[128];
+#define USBD_PRODUCT_STRING stringId
+#define USBD_CONFIGURATION_STRING     "MIDI Config"
+#define USBD_INTERFACE_STRING     "MIDI Interface"
 
 /* USER CODE BEGIN PRIVATE_DEFINES */
 
