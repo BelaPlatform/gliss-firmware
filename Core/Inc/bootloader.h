ec1df8a6f006428d411c4b7fcf69dab8c407a21a
Giulio Moro <giuliomoro@yahoo.it>
2023-06-28 02:34:32 +0200

    added some features to bootloader

--- Core/Inc/bootloader.h
+++ Core/Inc/bootloader.h
@@ -4,10 +4,21 @@ extern "C" {
 #endif
 #include "stm32g4xx_hal.h"
 
-__attribute__((noreturn)) void bootloaderResetTo();
+typedef enum {
+	// first entry actually unused here
+	kBootloaderMagicNone = 0,
+	kBootloaderMagicUserBootloader = 0xBE7A8007, // BOOT
+	kBootloaderMagicUserApplication = 0xBE7AE4EC, // EXEC
+	kBootloaderMagicSystemBootloader = 0xBE7A5457, // SYST
+} BootloaderResetDest_t;
+
+__attribute__((noreturn)) void bootloaderResetTo(BootloaderResetDest_t dest);
 void bootloaderJumpIfNeeded(RTC_HandleTypeDef* hrtc);
-int bootloaderShouldJump(RTC_HandleTypeDef* hrtc);
-__attribute__((noreturn)) void bootloaderJump(RTC_HandleTypeDef* hrtc);
+uint32_t bootloaderShouldJump(RTC_HandleTypeDef* hrtc);
+void bootloaderSetVector(void);
+void bootloaderJump(RTC_HandleTypeDef* hrtc, BootloaderResetDest_t to);
+int bootloaderIs();
+
 #ifdef __cplusplus
 }
 #endif
--- Core/Src/bootloader.c
+++ Core/Src/bootloader.c
@@ -1,16 +1,42 @@
 #include "bootloader.h"
 
 #define BOOTLOADER_BKP_REG RTC_BKP_DR0
-enum {
-	// first entry actually unused here
-	kBootloaderMagicRuntime = 0xBE7AE4EC, // EXEC
-	kBootloaderMagicSystemBootloader = 0xBE7A5457, // SYST
-};
 
 enum {
 	SYSTEM_BOOTLOADER_START = 0x1FFF0000, // for STM32G4, got this from AN2602 or 2.6.1 of the TRM
-	RUNTIME_START = 0x08000000,
+	USER_BOOTLOADER_START = 0x08000000,
+	USER_APPLICATION_START = 0x08010000,
 };
+
+extern void main();
+int bootloaderIs()
+{
+    if(USER_APPLICATION_START < USER_BOOTLOADER_START && (uint32_t)main > USER_BOOTLOADER_START)
+        return 1;
+    else if (USER_BOOTLOADER_START < USER_APPLICATION_START && (uint32_t)main < USER_APPLICATION_START)
+        return 1;
+    return 0;
+}
+
+void bootloaderSetVector()
+{
+    // set the interrupt vector address.
+    // should be called as soon as possible from main()
+
+    // The vector address is set to 0x08000000 by SystemInit, but
+    // we have to change it when starting the code at a
+    // different location. Unfortunately it cannot be set
+    // via Makefile or HAL (yet).
+    // See e.g.: https://github.com/STMicroelectronics/STM32CubeL0/pull/10 and related issues
+    uint32_t addr;
+    if(bootloaderIs())
+        addr = USER_BOOTLOADER_START;
+    else
+        addr = USER_APPLICATION_START;
+    // printf("main is %p. Vector is %p. Setting it to %p\n\r", main, (void*)SCB->VTOR, (void*)addr);
+    // a similar solution is mentioned in https://stackoverflow.com/a/28689746/2958741
+    SCB->VTOR = addr;
+}
 
 static void prepareJumpOrReset()
 {
@@ -43,25 +69,24 @@ static void writeToBackupRegister(RTC_HandleTypeDef* hrtc, uint32_t value)
 	HAL_PWR_DisableBkUpAccess();
 }
 
-__attribute__((noreturn)) void bootloaderResetTo()
+__attribute__((noreturn)) void bootloaderResetTo(BootloaderResetDest_t dest)
 {
-	uint32_t value = kBootloaderMagicSystemBootloader;
 	prepareJumpOrReset();
 	extern RTC_HandleTypeDef hrtc;
-	writeToBackupRegister(&hrtc, value);
+	writeToBackupRegister(&hrtc, dest);
 	HAL_NVIC_SystemReset();
 	while(1) // compensating for HAL_NVIC_SystemReset() not being marked as noreturn
 		;
 }
 
-__attribute__((noreturn)) void bootloaderJumpTo(uint32_t BootAddr)
+__attribute__((noreturn)) void bootloaderJumpToAddr(uint32_t BootAddr)
 {
 	// more from https://st.force.com/community/s/article/STM32H7-bootloader-jump-from-application
 	prepareJumpOrReset();
 
 	/* Set up the jump to bootloader address + 4 */
 	__attribute__((noreturn)) void (*SysMemBootJump)(void);
-	SysMemBootJump = (void*) /* cast to avoid compiler warning*/ (void (*)(void)) (*((uint32_t *) ((BootAddr + 4))));
+	SysMemBootJump = (void*)(*(uint32_t*)(BootAddr + 4)); // offset + 4 contains the address of the Reset_Handler
 
 	/* Set the main stack pointer to the bootloader stack */
 	__set_MSP(*(uint32_t *)BootAddr);
@@ -70,29 +95,51 @@ __attribute__((noreturn)) void bootloaderJumpTo(uint32_t BootAddr)
 	SysMemBootJump();
 }
 
-int bootloaderShouldJump(RTC_HandleTypeDef* hrtc) {
+uint32_t  bootloaderShouldJump(RTC_HandleTypeDef* hrtc) {
 	uint32_t value = HAL_RTCEx_BKUPRead(hrtc, BOOTLOADER_BKP_REG);
-	if(kBootloaderMagicSystemBootloader == value)
-		return 1;
-	else
-		return 0;
+	switch(value)
+	{
+	case kBootloaderMagicUserBootloader:
+	case kBootloaderMagicUserApplication:
+	case kBootloaderMagicSystemBootloader:
+		return value;
+	}
+	return 0;
 }
 
-__attribute__((noreturn)) void bootloaderJump(RTC_HandleTypeDef* hrtc)
+void bootloaderJump(RTC_HandleTypeDef* hrtc, BootloaderResetDest_t to)
 {
 	// restore content so we don't get stuck here forever
-	writeToBackupRegister(hrtc, kBootloaderMagicRuntime);
+	writeToBackupRegister(hrtc, kBootloaderMagicUserApplication);
 	// ensure elsewhere that the host is forced to
 	// re-enumerate the peripheral so it can detect it now
 	// is a DFU device.
-	bootloaderJumpTo(SYSTEM_BOOTLOADER_START);
+	uint32_t dest = 0;
+	switch(to)
+	{
+	case kBootloaderMagicUserApplication:
+		dest = USER_APPLICATION_START;
+		break;
+	case kBootloaderMagicUserBootloader:
+		dest = USER_BOOTLOADER_START;
+		break;
+	case kBootloaderMagicSystemBootloader:
+		dest = SYSTEM_BOOTLOADER_START;
+		break;
+	case kBootloaderMagicNone:
+		dest = 0;
+		break;
+	}
+	if(dest)
+		bootloaderJumpToAddr(dest);
 }
 
 // this must be called before any DMA/Timer/IRQ configuration takes place
 void bootloaderJumpIfNeeded(RTC_HandleTypeDef* hrtc) {
 	// do jump to bootloader or runtime if the backup register indicates
 	// that we are not on the right one.
-	if(bootloaderShouldJump(hrtc))
-		bootloaderJump(hrtc);
+	uint32_t to = bootloaderShouldJump(hrtc);
+	if(to)
+		bootloaderJump(hrtc, to);
 	// otherwise keep going as we are
 }
