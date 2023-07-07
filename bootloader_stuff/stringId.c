c3ac94cdae6d3f1b919cf91f872cf5c0a5532188
Giulio Moro <giuliomoro@yahoo.it>
2023-07-07 06:22:54 -0500

    stringId alternative: TODO: fix

--- bootloader_stuff/stringId.c
+++ bootloader_stuff/stringId.c
@@ -1,2 +1,6 @@
 #include "stringId.h"
+#ifdef BOOTLOADER_ONLY
+char stringId[256] = "GLISS-BOOTLOADER";
+#else
 char stringId[256] __attribute__((section(".stringIdSec"), used)) = "Gliss";
+#endif // BOOTLOADER_ONLY
