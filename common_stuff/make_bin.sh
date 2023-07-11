67fb700f144d2d7e4928262a2b2e85583f69db29
Giulio Moro <giuliomoro@yahoo.it>
2023-07-10 22:51:49 -0500

    make_bin: also rebuild the projects

--- common_stuff/make_bin.sh
+++ common_stuff/make_bin.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-set -e
+set -ex
 
+p=$(pwd)
 bootloader_k=8
 flasher_k=56
 application_k=320
-
-dd if=../Bootloader/G491KE-TrillRack.bin of=bootloader-full.bin count=$bootloader_k bs=1k
-dd if=../Flasher/G491KE-TrillRack.bin of=fl-full.bin count=$bootloader_k bs=1k
-dd if=../Debug/G491KE-TrillRack.bin of=application-full.bin count=$application_k bs=1k
-
 name=G491KE-TrillRack.bin
+
+for a in Bootloader Flasher Debug; do
+	cd ../$a && make $name
+done
+cd $p
+
 cp ../Bootloader/$name .
 dd if=../Flasher/$name of=$name oseek=$bootloader_k iseek=0 bs=1k
 dd if=../Debug/$name of=$name oseek=$((bootloader_k+flasher_k)) iseek=0 bs=1k
