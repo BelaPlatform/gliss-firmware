ac7e30c2361baf6bad78f0c38fcdaf09df7eb630
Giulio Moro <giuliomoro@yahoo.it>
2023-09-18 12:37:08 -0500

    make_bin.sh: indepedent of working directory

--- common_stuff/make_bin.sh
+++ common_stuff/make_bin.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -ex
 
-p=$(pwd)
+p=$(dirname $0)
+cd $p
+
 bootloader_k=8
 flasher_k=56
 application_k=320
 name=G491KE-TrillRack.bin
 
 for a in Bootloader Flasher Debug; do
-	cd ../$a && make $name
+	make -C ../$a $name
 done
-cd $p
 
 cp ../Bootloader/$name .
 dd if=../Flasher/$name of=$name oseek=$bootloader_k iseek=0 bs=1k
